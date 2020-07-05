8685e6a3d46813e766562f05a5066b2607f6f4dd
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-05 12:14:37 +0200

    setup new instance of Unsplash

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const config = require('universal-config')
 const Unsplash = require('unsplash-js').default
 const toJson = require('unsplash-js').toJson
 const express = require('express')
+
+const unsplash = new Unsplash({
+  applicationId: config.get('APPLICATION_ID'),
+  secret: config.get('SECRET'),
+  callbackUrl: config.get('CALLBACK_URL')
+})
