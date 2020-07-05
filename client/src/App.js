27898d770277b62edab55a16fcabdc95ac3792c8
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-05 12:34:52 +0200

    get html from codepen

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,29 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <div id='root'>
+      <div class='hero is-fullheight is-bold is-info'>
+        <div class='hero-body'>
+          <div class='container'>
+            <div class='header content'>
+              <h2 class='subtitle is-6'>Code Challenge #16</h2>
+              <h1 class='title is-1'>
+                Infinite Scroll Unsplash Code Challenge
+              </h1>
+            </div>
+
+            <img
+              src='https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif'
+              alt='loading'
+            />
+
+            <div class='images'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
