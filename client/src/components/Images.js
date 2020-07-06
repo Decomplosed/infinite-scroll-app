e397fd053eabde4c094d09839fedd36dfc12ff5b
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 09:08:29 +0200

    defined a state

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class Images extends Component {
+  state = {
+    
+  }
+
   render() {
     return <div></div>
   }
