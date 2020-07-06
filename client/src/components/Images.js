eeb4830721e275655d5d467dd9585105d6e85944
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 09:07:25 +0200

    im-orted axios

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class Images extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div></div>
   }
 }
 
