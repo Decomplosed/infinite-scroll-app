c005090ab3e28af41de5aa336a985a7b1133a2f3
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 09:07:59 +0200

    imported infinite scroll

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class Images extends Component {
   render() {
