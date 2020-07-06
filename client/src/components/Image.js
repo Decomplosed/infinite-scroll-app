253de80f116dfce3ea191f6573ef12fa98f48724
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-06 09:35:12 +0200

    added alt to the images

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Image = ({ image }) => {
-  return <img className='single-photo' src={image.urls.thumb} alt='' />
+  return (
+    <img
+      className='single-photo'
+      src={image.urls.thumb}
+      alt={image.alt_description || ''}
+    />
+  )
 }
 
 export default Image
